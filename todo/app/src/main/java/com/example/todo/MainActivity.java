e1175d74019e55925ff77716f77cb55484a0ecdd
edbgt <et571fou@htwg-konstanz.de>
2021-05-01 16:10:57 +0200

    cleaned up imports

--- todo/app/src/main/java/com/example/todo/MainActivity.java
+++ todo/app/src/main/java/com/example/todo/MainActivity.java
@@ -1,25 +1,18 @@
 package com.example.todo;
 
 import androidx.annotation.NonNull;
-import androidx.appcompat.app.AppCompatActivity;
 
-import android.app.AlertDialog;
 import android.app.ListActivity;
-import android.app.NotificationChannel;
-import android.app.NotificationManager;
 import android.content.Context;
 import android.content.Intent;
-import android.os.Build;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.ContextMenu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.ListView;
-import android.widget.Toast;
 
 import com.example.todo.command.Complete;
 import com.example.todo.command.Delete;
@@ -29,7 +22,6 @@ import com.wdullaer.swipeactionadapter.SwipeActionAdapter;
 import com.wdullaer.swipeactionadapter.SwipeDirection;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class MainActivity extends ListActivity implements SwipeActionAdapter.SwipeActionListener {
