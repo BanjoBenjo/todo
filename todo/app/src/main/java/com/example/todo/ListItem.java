e61f0105200941f7ab524441d8dc640eec33796d
edbgt <et571fou@htwg-konstanz.de>
2021-07-04 11:42:39 +0200

    commented list item, list task and cleaned imports in main activity

--- todo/app/src/main/java/com/example/todo/ListItem.java
+++ todo/app/src/main/java/com/example/todo/ListItem.java
@@ -1,6 +1,10 @@
 package com.example.todo;
 
 public class ListItem implements Comparable<ListItem>{
+    /**
+     * List items in list tasks. Originally meant as a shopping list, items are checkable. Checked
+     * state is displayed on the right by checking box.
+     */
     private boolean checked;
     private String name;
 
@@ -21,10 +25,14 @@ public class ListItem implements Comparable<ListItem>{
         checked = !checked;
     }
 
-    public void setChecked(Boolean check) { checked = check; }
+    public void setChecked(Boolean check) {
+        checked = check;
+    }
+
     public void setName(String newName) {
         name = newName;
     }
+
     public String getName(){
         return name;
     }
